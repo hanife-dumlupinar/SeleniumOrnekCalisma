d760c085015fb87cdba547a6c72480c01c70475a
hanife-dumlupinar <hnfdmlpnr@gmail.com>
2024-11-05 21:20:01 +0300

    java class name changed

--- src/test/java/A01_BookAddDelete.java
+++ src/test/java/A01_BookAddDelete.java
@@ -1,5 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,10 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.JavascriptExecutor;
 
-
-import java.security.Key;
-
-public class A01_kitap {
+public class A01_BookAddDelete {
 
 
     public static void main(String[]args) throws InterruptedException {
